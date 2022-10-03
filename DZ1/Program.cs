87ba22e8b27f6cc046d8a6085a2c19e81d3b51f8
Nimere1990 <nimereiv@gmail.com>
2022-10-03 19:52:36 +0300

    Готовое ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+Console.WriteLine("Введите размерность массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Ваш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+    if (numbers[i] % 2 == 0)
+        count++;
+
+Console.WriteLine($"Всего чисел: {numbers.Length} , чётные: {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
